fb0ef38fdb0cba4aa40d3b639dbeed8739aaadce
Florian Guigue <florian.guigue@etu.univ-lyon1.fr>
2018-10-18 10:25:25 +0200

    article model

--- src/app/models/article.ts
+++ src/app/models/article.ts
@@ -1,9 +1,9 @@
 export class Article {
   private id_article: number;
   private id_domaine: number;
-  private titre: String;
-  private resume: String;
+  private titre: string;
+  private resume: string;
   private prix: number;
-  private date_article: String;
-  private fichier: String;
+  private date_article: string;
+  private fichier: string;
 }
